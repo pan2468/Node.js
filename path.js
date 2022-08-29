945c6652cd639e90114db5f0b5d14f1f56bdc747
임채성 <pan1129@naver.com>
2022-08-29 19:14:53 +0900

    commit

---
+++ path.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const string = __filename;
+
+console.log('path.sep:', path.sep);
+console.log('path.delimiter:',path.delimiter);
+
+console.log('-------------------------------');
+console.log('path.dirname():',path.dirname(string));
+console.log('path.extname():',path.extname(string));
+console.log('path.basename():',path.basename(string));
+
+
+
